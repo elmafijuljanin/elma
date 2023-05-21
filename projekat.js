e25a0e2e2e1204e995a5b014cae68237955ff1f2
elmafijuljanin <134229649+elmafijuljanin@users.noreply.github.com>
2023-05-21 21:44:22 +0200

    Add files via upload

---
+++ projekat.js
@@ -0,0 +1,5 @@
+document.getElementsByClassName("prvijs")[0].style.color="rgb(172, 20, 117)";
+document.getElementsByClassName("drugijs")[0].style.color="rgb(172, 20, 117)";
+document.getElementsByClassName("trecijs")[0].style.color="rgb(172, 20, 117)";
+document.getElementsByClassName("cetvrtijs")[0].style.color="rgb(172, 20, 117)";
+document.getElementsByClassName("petijs")[0].style.color="rgb(172, 20, 117)";
